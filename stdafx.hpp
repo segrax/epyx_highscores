e2b1a5703472dc3b34ad123acff9bcfdc2ba4e2f
Robert Crossfield <robcrossfield@gmail.com>
2019-06-15 06:43:08 +1000

    Add some notices

--- stdafx.hpp
+++ stdafx.hpp
@@ -1,3 +1,27 @@
+/*
+
+Copyright (c) 2019 Robert Crossfield
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
 #include <iostream>
 #include <vector>
 #include <memory>
@@ -7,12 +31,10 @@ typedef std::shared_ptr<std::vector<uint8_t>> tSharedBuffer;
 #include "json.hpp"
 using Json = nlohmann::json;
 
-
 #include "d64.hpp"
 #include "resource.hpp"
 #include "epyx_worldrecords.hpp"
 #include "records.hpp"
-
 
 extern std::shared_ptr<cResource> gResources;
 extern std::vector<sKnownGame> mKnownGames;
